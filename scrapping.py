98d7bfba5026c661c2424110d35805f37ee7e71a
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-08-16 00:36:32 -0700

    new

--- scrapping.py
+++ scrapping.py
@@ -1,28 +1,28 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup 
-
-#writing function
-def write(a):
-    f = open('test.html',"a")
+from bs4 import BeautifulSoup as soup  # HTML data structure
+from urllib.request import urlopen as uReq  # Web client
+# html file writing function
+def htmlWrite(a):
+    f= open("test.html","a")
     f.write(str(a))
     f.close()
 
+# URl to web scrap from.
+# in this example we web scrap graphics cards from Newegg.com
+page_url = "http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=-1&IsNodeId=1&Description=GTX&bop=And&Page=1&PageSize=36&order=BESTMATCH"
 
+# opens the connection and downloads html page from url
+uClient = uReq(page_url)
 
-
-myURL= "https://www.newegg.com/global/in-en/Storage/EventSaleStore/ID-878?cm_sp=TOPVGAStore"
-
-#opening up the connection, grabbing the page
-
-uClient = uReq(myURL)
-pageHTML= uClient.read()
+# parses html into a soup data structure to traverse html
+# as if it were a json data type.
+page_soup = soup(uClient.read(), "html.parser")
 uClient.close()
 
+# finds each product from the store page
+containers = page_soup.findAll("div", {"class": "item-container"})
 
-pageSOUP = soup(pageHTML,"html.parser")
+print(len(containers))
+container = containers[0]
+htmlWrite(container.a.div)
 
-#grab each product
-containers = pageSOUP.findAll("div",{"class":"item-cell"})
 
-container = containers[1]
-write(container.a )
